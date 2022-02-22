7062c6456ad08b0fdb9f13ba8592b49747c00263
Dzuelu <Dzuelu@users.noreply.github.com>
2022-02-22 18:00:28 -0500

    Add first draft of update script

---
+++ bin/update.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# This 'app' is run on a raspberryPi. To update I have to ssh into the Pi and run this script.
+# Eventually I would like to find a way to redeploy this to the pi with a github action
+# or similar but for now this will do.
+
+# Stop old image
+docker stop DiscordBot
+
+# Update the local code
+git pull
+
+# Build docker image
+docker build -t DiscordBot:latest -f- . <<EOF
+FROM node:16
+
+EXPOSE 8080
+
+WORKDIR /app
+COPY package.json ./
+COPY yarn.lock ./
+RUN yarn install --frozen-lockfile
+
+# Bundle app source
+COPY . .
+
+# Update rest endpoints
+RUN yarn rest
+
+# Run the discord bot
+CMD [ "yarn", "client" ]
+EOF
+
+# Run new image
+
