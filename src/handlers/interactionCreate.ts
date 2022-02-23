6313580f1d0b7373a24edff1f99fb5750b9565e5
Dzuelu <Dzuelu@users.noreply.github.com>
2022-02-22 20:31:55 -0500

    Use interaction.commandName to determine command

--- src/handlers/interactionCreate.ts
+++ src/handlers/interactionCreate.ts
@@ -3,11 +3,10 @@ import { Interaction, CacheType } from 'discord.js';
 
 export const interactionCreate = async (interaction: Interaction<CacheType>): Promise<void> => {
   console.log('starting interactionCreate');
-  if (!interaction.isCommand() || interaction.command == null) return;
-  const interactionName = interaction.command.name;
-  console.log(`interactionName: ${interactionName}`);
+  if (!interaction.isCommand()) return;
+  console.log(`interactionName: ${interaction.commandName}`);
 
-  const currentCommand = commandList.find(command => command.slashCommand.name === interactionName);
+  const currentCommand = commandList.find(command => command.slashCommand.name === interaction.commandName);
 
   if (currentCommand) {
     try {
