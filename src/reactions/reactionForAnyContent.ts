b824b6738d641edadb2091de17b6fe98b8ad6ce1
Dzuelu <Dzuelu@users.noreply.github.com>
2022-09-20 18:26:19 -0400

    split emoji names with caps for reactions

--- src/reactions/reactionForAnyContent.ts
+++ src/reactions/reactionForAnyContent.ts
@@ -9,16 +9,12 @@ const reactWith = async (reactionId: string, keyWords: string[], message: Messag
 };
 
 export const reactionForAnyContent = async (message: Message<boolean>): Promise<void> => {
-  message.client.emojis.cache.forEach(emoji => {
-    if (emoji.name && message.content.includes(emoji.name)) {
-      console.log(`message id (${message.id}) reacted with ${emoji.name} (${emoji.id})`);
-      message.react(emoji.id);
-    }
-  });
   await Promise.all([
+    ...message.client.emojis.cache.map(emoji =>
+      emoji.name ? reactWith(emoji.id, emoji.name.split(/(?=[A-Z])/), message) : Promise.resolve()
+    ),
     reactWith('809906065247436830', ['brain', 'bot', 'coc'], message),
     reactWith('676623420997238784', ['69', 'nice'], message),
-    reactWith('636992777099542530', ['jojo'], message),
     reactWith('854864449101103105', ['cow'], message),
     reactWith('854863643115520020', ['but', 'bun'], message)
   ]);
