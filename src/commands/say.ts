397cda76d24a30092ce8ef0fe258abc41c07c861
Dzuelu <Dzuelu@users.noreply.github.com>
2022-09-23 19:17:54 -0400

    Add back ephemeral reply...

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -3,12 +3,14 @@ import { PermissionsBitField } from 'discord.js';
 import { CommandItem } from 'models';
 
 export const say: CommandItem = {
-  execute: async (interaction): Promise<void> => {
+  execute: async (interaction, client): Promise<void> => {
     if (!interaction.isChatInputCommand()) return;
     const input = interaction.options.getString('input');
 
-    if (input && interaction.channel?.isTextBased()) {
-      await interaction.channel.send(input);
+    const interactionChannel = client.channels.cache.get(interaction.channelId);
+    if (interactionChannel && input && interactionChannel.isTextBased()) {
+      await interaction.reply({ content: 'It is done.', ephemeral: true });
+      await interactionChannel.send(input);
     }
   },
   slashCommand: new SlashCommandBuilder()
