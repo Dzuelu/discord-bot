81c1c2300aef21df8ed3ad1ccaf22e32786eac2d
Dzuelu <Dzuelu@users.noreply.github.com>
2022-09-23 20:52:42 -0400

    Add welcome message file

---
+++ src/handlers/memberAdded.ts
@@ -0,0 +1,6 @@
+import { Client, GuildMember } from 'discord.js';
+
+export const memberAdded = async (member: GuildMember, server: Client): Promise<void> => {
+  const generalChannel = server.channels.cache.get('223498053246648321'); // wrinkle brains general
+  await (generalChannel?.isTextBased() ? generalChannel.send(`Welcome <@${member.id}>!`) : Promise.resolve());
+};
