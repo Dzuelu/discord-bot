3be52490d56d1c24824dfa094f2bf11b9cf79eec
Dzuelu <Dzuelu@users.noreply.github.com>
2022-09-24 23:22:10 -0400

    Clear guild specific commands

--- src/rest.ts
+++ src/rest.ts
@@ -6,6 +6,9 @@ import { discordClientId, discordToken } from 'utils';
 const rest = new REST({ version: '9' }).setToken(discordToken());
 const commands = commandList.map(commandItem => commandItem.slashCommand.toJSON());
 
+// Clear old guild specific commands, only needs to be run once
+rest.put(Routes.applicationGuildCommands(discordClientId(), '223498053246648321'), { body: [] });
+
 rest
   .put(Routes.applicationCommands(discordClientId()), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
