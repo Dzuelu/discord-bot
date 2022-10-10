50fd11d3fa54361c9eb6735bd226a7abb39f3a41
Dzuelu <Dzuelu@users.noreply.github.com>
2022-10-10 12:31:20 -0400

    Download video, upload to discord and delete temp video

--- src/reactions/urls/tiktokUrl.ts
+++ src/reactions/urls/tiktokUrl.ts
@@ -1,13 +1,31 @@
 import { Message } from 'discord.js';
 import { fetchVideo } from 'tiktok-scraper-ts';
+import { get } from 'https';
+import { createWriteStream, unlinkSync } from 'fs';
+
+const downloadUrl = async (filename: string, url: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    get(url, res => {
+      const stream = createWriteStream(`./temp/${filename}`);
+      stream.on('error', error => reject(error));
+      stream.on('finish', () => {
+        stream.close();
+        resolve();
+      });
+      res.pipe(stream);
+    });
+  });
+};
 
 export const tiktokUrl = async (url: string, message: Message<boolean>): Promise<void> => {
   console.log(`tiktok url detected: ${url}`);
   try {
     if (message.channelId === '1028811140630843434') {
       // Temp channel check
-      const tiktokVideo = await fetchVideo(url);
-      await message.channel.send({ files: [tiktokVideo.downloadURL] });
+      const video = await fetchVideo(url);
+      await downloadUrl(video.id, video.downloadURL);
+      await message.channel.send({ files: [`./temp/${video.id}`] });
+      unlinkSync(`./temp/${video.id}`);
     }
   } catch (error) {
     console.error(error);
