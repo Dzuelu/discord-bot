a33dc152f50de5641da25d9660a1b64577daa879
Dzuelu <Dzuelu@users.noreply.github.com>
2022-10-10 11:52:22 -0400

    Use array of objects instead of object for regex literal

--- src/reactions/checkForUrls.ts
+++ src/reactions/checkForUrls.ts
@@ -1,17 +1,21 @@
 import { Message } from 'discord.js';
 import { tiktokUrl } from './urls/tiktokUrl';
 
-const matchers: { [key: string]: (url: string, message: Message<boolean>) => Promise<void> } = {
-  /* eslint-disable @typescript-eslint/naming-convention */
-  '/(?:(?:https?)://.*.tiktok.com/)/': tiktokUrl
-  /* eslint-enable @typescript-eslint/naming-convention */
-};
+const matchers: {
+  fn: (url: string, message: Message<boolean>) => Promise<void>;
+  regex: RegExp;
+}[] = [
+  {
+    fn: tiktokUrl,
+    regex: /(?:(?:https?):\/\/.*.tiktok.com\/)/
+  }
+];
 
 export const checkForUrls = async (message: Message<boolean>): Promise<void> => {
   await Promise.all(
     message.content.split(' ').map(possibleUrl => {
-      const foundMatcher = Object.keys(matchers).find(regex => possibleUrl.match(regex));
-      return foundMatcher ? matchers[foundMatcher](possibleUrl, message) : Promise.resolve();
+      const foundMatcher = matchers.find(matchFinder => possibleUrl.match(matchFinder.regex));
+      return foundMatcher ? foundMatcher.fn(possibleUrl, message) : Promise.resolve();
     })
   );
 };
