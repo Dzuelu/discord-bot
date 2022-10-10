c70b958c5a925de86c4f0372b4bcb16a833a66c2
Dzuelu <Dzuelu@users.noreply.github.com>
2022-10-10 12:49:52 -0400

    Remove channel restriction, and some cleanup

--- src/reactions/urls/tiktokUrl.ts
+++ src/reactions/urls/tiktokUrl.ts
@@ -6,7 +6,7 @@ import { createWriteStream, unlinkSync, mkdtempSync } from 'fs';
 const downloadUrl = async (filename: string, url: string): Promise<string> =>
   new Promise((resolve, reject) => {
     get(url, res => {
-      const fileName = `${mkdtempSync('videos')}/${filename}.mp4`;
+      const fileName = `${mkdtempSync('videos')}/${filename}`;
       const stream = createWriteStream(fileName);
       stream.on('error', error => reject(error));
       stream.on('finish', () => {
@@ -20,17 +20,12 @@ const downloadUrl = async (filename: string, url: string): Promise<string> =>
 export const tiktokUrl = async (url: string, message: Message<boolean>): Promise<void> => {
   console.log(`tiktok url detected: ${url}`);
   try {
-    if (message.channelId === '1028811140630843434') {
-      // Temp channel check
-      const video = await fetchVideo(url);
-      console.log('downloading file...');
-      const file = await downloadUrl(video.id, video.downloadURL);
-      console.log('sending file to discord...');
-      await message.channel.send({ files: [file] });
-      console.log('deleting file...');
-      unlinkSync(file);
-    }
+    const video = await fetchVideo(url);
+    // Hack, should instead check the encoding in url. But this should work for now
+    const file = await downloadUrl(`${video.id}.mp4`, video.downloadURL);
+    await message.channel.send({ files: [file] });
+    unlinkSync(file);
   } catch (error) {
-    console.error(error);
+    console.error('tiktokUrl error', { error, url });
   }
 };
