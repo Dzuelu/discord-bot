d2496d4d0234e16a0bf10c45badd1a73f4949720
Dzuelu <Dzuelu@users.noreply.github.com>
2022-10-10 12:35:38 -0400

    Add log messages for file progress

--- src/reactions/urls/tiktokUrl.ts
+++ src/reactions/urls/tiktokUrl.ts
@@ -3,19 +3,19 @@ import { fetchVideo } from 'tiktok-scraper-ts';
 import { get } from 'https';
 import { createWriteStream, unlinkSync } from 'fs';
 
-const downloadUrl = async (filename: string, url: string): Promise<void> => {
-  return new Promise((resolve, reject) => {
+const downloadUrl = async (filename: string, url: string): Promise<string> =>
+  new Promise((resolve, reject) => {
     get(url, res => {
-      const stream = createWriteStream(`./temp/${filename}`);
+      const fileName = `./tmp/${filename}`;
+      const stream = createWriteStream(fileName);
       stream.on('error', error => reject(error));
       stream.on('finish', () => {
         stream.close();
-        resolve();
+        resolve(fileName);
       });
       res.pipe(stream);
     });
   });
-};
 
 export const tiktokUrl = async (url: string, message: Message<boolean>): Promise<void> => {
   console.log(`tiktok url detected: ${url}`);
@@ -23,9 +23,12 @@ export const tiktokUrl = async (url: string, message: Message<boolean>): Promise
     if (message.channelId === '1028811140630843434') {
       // Temp channel check
       const video = await fetchVideo(url);
-      await downloadUrl(video.id, video.downloadURL);
-      await message.channel.send({ files: [`./temp/${video.id}`] });
-      unlinkSync(`./temp/${video.id}`);
+      console.log('downloading file...');
+      const file = await downloadUrl(video.id, video.downloadURL);
+      console.log('sending file to discord...');
+      await message.channel.send({ files: [file] });
+      console.log('deleting file...');
+      unlinkSync(file);
     }
   } catch (error) {
     console.error(error);
