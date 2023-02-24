cbedb32babbb1a4ce235749e6e3779c1fc18160d
Dzuelu <Dzuelu@users.noreply.github.com>
2023-02-23 19:04:44 -0500

    Fix unit test

--- spec/unit/commands/ping.spec.ts
+++ spec/unit/commands/ping.spec.ts
@@ -3,7 +3,10 @@ import { CommandInteraction, CacheType, Client } from 'discord.js';
 
 describe('ping', () => {
   it('replies pong', async () => {
-    const interaction = { reply: jest.fn() } as unknown as CommandInteraction<CacheType>;
+    const interaction = {
+      channel: { isTextBased: () => true, isVoiceBased: () => false, sendTyping: jest.fn() },
+      reply: jest.fn()
+    } as unknown as CommandInteraction<CacheType>;
 
     await ping.execute(interaction, jest.fn() as unknown as Client);
 
