9128c5d42f36ff7fc0087f3811af702e6f8e388e
Dzuelu <Dzuelu@users.noreply.github.com>
2023-07-16 10:45:35 -0400

    increase random bot reaction

--- src/reactions/randomReaction.ts
+++ src/reactions/randomReaction.ts
@@ -4,5 +4,5 @@ import { chance } from 'utils';
 // Reacts with a random reaction, randomly
 export const randomReaction = async (message: Message<boolean>, server: Client): Promise<void> => {
   const randomEmoji = server.emojis.cache.random();
-  await chance(0.01, async () => randomEmoji && message.react(randomEmoji));
+  await chance(0.1, async () => randomEmoji && message.react(randomEmoji));
 };
