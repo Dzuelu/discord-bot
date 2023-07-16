c8cdd918f102354215c21c3d0ff4406c54e60413
Dzuelu <Dzuelu@users.noreply.github.com>
2023-07-16 10:48:26 -0400

    fix unit test to be more stable

--- spec/unit/utils/sleep.spec.ts
+++ spec/unit/utils/sleep.spec.ts
@@ -3,7 +3,7 @@ import { sleep } from 'utils';
 describe('sleep', () => {
   it('first item for very close to 0 random value', async () => {
     const start = Date.now();
-    await sleep(100);
+    await sleep(110); // test may fail at 100 if calculation was fast (ie got 99 sometimes)
     expect(Date.now() - start).toBeGreaterThanOrEqual(100);
   });
 });
