f8533f3ba3217f6c24eaf9da5a197084c8819d9d
Dzuelu <Dzuelu@users.noreply.github.com>
2023-10-30 17:59:30 -0400

    lint config change

--- .eslintrc.js
+++
@@ -1,3 +0,0 @@
-const defaultLint = require('eslint-config');
-
-module.exports = defaultLint;
---
+++ .eslintrc.ts
@@ -0,0 +1,7 @@
+import { ESLint } from 'eslint';
+
+const config: ESLint.ConfigData = {
+  extends: ['dzuelu-eslint-config']
+};
+
+export default config;
