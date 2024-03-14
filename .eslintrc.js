a4f56254aa68e862b3990491300aa7cb8e3ac89f
Dzuelu <Dzuelu@users.noreply.github.com>
2024-03-14 12:37:47 -0400

    fix more lint and upgrade packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 const defaultLint = require('dzuelu-eslint-config');
 
+defaultLint.rules['node/no-missing-import'] = ["error", {
+  "allowModules": ["@discordjs/rest"],
+  "tryExtensions": [".js", ".ts"]
+}];
+
 module.exports = defaultLint;
